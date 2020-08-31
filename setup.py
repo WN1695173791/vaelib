65ce86552861a79ccc521caca5921fdaa3f8dc2d
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-08-31 10:20:31 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    "torch==1.6.0",
+    "torch>=1.6.0",
 ]
+
+
+extras_require = {
+    "dev": ["pytest"]
+}
 
 
 setup(
@@ -13,4 +18,5 @@ setup(
     description="VAE models by PyTorch",
     packages=find_packages(),
     install_requires=install_requires,
+    extras_require=extras_require,
 )
