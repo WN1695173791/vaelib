3d821017aba19be7999de865de7238c6a1305ebc
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-10-30 10:38:59 +0900

    Add trainer

--- vaelib/__init__.py
+++ vaelib/__init__.py
@@ -1,11 +1,12 @@
-from .avb import AVB  # noqa
-from .base import (  # noqa
+from vaelib.avb import AVB  # noqa
+from vaelib.base import (  # noqa
     BaseVAE,
     kl_divergence_normal,
     kl_divergence_normal_diff,
     nll_bernoulli,
     nll_logistic,
 )
-from .betavae import BetaVAE  # noqa
-from .nouveauvae import NouveauVAE  # noqa
-from .scheduler import LinearAnnealer  # noqa
+from vaelib.betavae import BetaVAE  # noqa
+from vaelib.experiment import Trainer  # noqa
+from vaelib.nouveauvae import NouveauVAE  # noqa
+from vaelib.scheduler import LinearAnnealer  # noqa
